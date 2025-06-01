3f82f0980dc3e9245cc695538fdfba9f8c14c8b1
oBoolt <bernardo.patto.pinheiro@gmail.com>
2025-06-01 00:19:36 -0300

    feat(nvim-treesitter): update from 'master' to 'main' branch

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,14 +1,10 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    lazy = false,
     dependencies = { 
         {'nvim-lua/plenary.nvim'},
         {'BurntSushi/ripgrep'},
     },
-    -- opts = {
-    --     defaults = {
-    --         file_ignore_patterns = { ".git/" },
-    --     },
-    -- },
     config = function()
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', function() builtin.find_files({ hidden = true, file_ignore_patterns = { ".git/" } }) end, {})
@@ -16,5 +12,13 @@ return {
         vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
+
+        require('telescope').setup {
+            defaults = {
+                preview = {
+                    treesitter = false
+                }
+            },
+        }
     end,
 }
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,26 @@
 return {
     'nvim-treesitter/nvim-treesitter',
-    main = 'nvim-treesitter.configs',
-    opts = {
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "rust", "javascript", "typescript", "java" },
-        sync_install = false,
-        auto_install = true,
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = false,
-        },
-    },
+    lazy = false,
+    branch = 'main',
     build = ':TSUpdate',
+    config = function() 
+        langs = { 'rust', 'javascript', 'typescript', 'java', 'javadoc', 'css', 'scss' }
+        require('nvim-treesitter').install(langs);
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = langs,
+            callback = function()
+                vim.treesitter.start()
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
+        });
+    end,
+    -- opts = {
+    --     ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "rust", "javascript", "typescript", "java" },
+    --     sync_install = false,
+    --     auto_install = true,
+    --     highlight = {
+    --         enable = true,
+    --         additional_vim_regex_highlighting = false,
+    --     },
+    -- },
 }
