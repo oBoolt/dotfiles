baaef0247e9ff3d67dfa023edf33bfc627782c5a
oBoolt <bernardo.patto.pinheiro@gmail.com>
2025-07-12 12:35:20 -0300

    feat(nvim.fugitive): remove fugitive plugin in favor of only using git cli

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -29,12 +29,6 @@ return {
         end,
     },
     {
-        'tpope/vim-fugitive',
-        init = function()
-            vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-        end
-    },
-    {
         'MeanderingProgrammer/render-markdown.nvim',
         dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
         -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
