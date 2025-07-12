cece2dc83a2c4552a264f64ff32dcbab000198a0
oBoolt <bernardo.patto.pinheiro@gmail.com>
2025-07-12 12:09:51 -0300

    feat(nvim.markdown): change from 'markdown-preview.nvim' to 'render-markdown.nvim'

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -34,5 +34,14 @@ return {
             vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
         end
     },
+    {
+        'MeanderingProgrammer/render-markdown.nvim',
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {},
+    }
 }
 
