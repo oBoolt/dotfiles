5ad6e96fc215bcbcafc532a0fecf04e703144b39
oBoolt <bernardo.patto.pinheiro@gmail.com>
2026-02-23 18:43:01 -0300

    feat(config.nvim): add support for c/c++ debugger

---
+++ config/nvim/lua/plugins/dap.lua
@@ -0,0 +1,64 @@
+return {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+        "rcarriga/nvim-dap-ui",
+        "nvim-neotest/nvim-nio"
+    },
+    config = function()
+        local dap = require("dap");
+        local dapui = require("dapui");
+        local mason_path = vim.fn.stdpath("data") .. "/mason"
+
+        dapui.setup()
+
+        dap.adapters.cppdbg = {
+            id = 'cppdbg',
+            type = 'executable',
+            command = mason_path .. "/bin/OpenDebugAD7",
+        }
+
+        dap.configurations.cpp = {
+            {
+                name = "Launch file",
+                type = "cppdbg",
+                request = "launch",
+                program = function()
+                    return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                end,
+                cwd = '${workspaceFolder}',
+                stopAtEntry = true,
+            },
+            {
+                name = 'Attach to gdbserver :1234',
+                type = 'cppdbg',
+                request = 'launch',
+                MIMode = 'gdb',
+                miDebuggerServerAddress = 'localhost:1234',
+                miDebuggerPath = '/usr/bin/gdb',
+                cwd = '${workspaceFolder}',
+                program = function()
+                    return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                end,
+            },
+        }
+
+        dap.configurations.c = dap.configurations.cpp
+        dap.configurations.rust = dap.configurations.cpp
+
+        dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
+        end
+
+        vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
+        vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+    end
+}
